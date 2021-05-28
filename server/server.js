5d34b168068097a817cda890e64796b6ec85a703
hobbit <rusikarm3@gmail.com>
2021-05-28 23:59:08 +0300

    add routes

--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.listen(3000); 
+const auth = require('./routes/auth');
+
+app.use('/', auth.router);
 
 app.get('/', (req, res) => {
     console.log("hello");
 })
 
 module.exports = app;
+
+app.listen(3000); 
