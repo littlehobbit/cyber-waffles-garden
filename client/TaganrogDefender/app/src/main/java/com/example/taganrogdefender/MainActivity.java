4bd3c6b6a2689abe2d94d1f3b826ea9c00ca39f7
StadndartAccount <72969531+StadndartAccount@users.noreply.github.com>
2021-05-28 21:51:03 +0300

    до-добавления-okhttpclient

--- client/TaganrogDefender/app/src/main/java/com/example/taganrogdefender/MainActivity.java
+++ client/TaganrogDefender/app/src/main/java/com/example/taganrogdefender/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.taganrogdefender;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity  {
+
+    Boolean have_passport = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +16,13 @@ public class MainActivity extends AppCompatActivity  {
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        if(have_passport) {
+            findViewById(R.id.buy_passport_block).setVisibility(View.GONE);
+            findViewById(R.id.passport_block).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.buy_passport_block).setVisibility(View.VISIBLE);
+            findViewById(R.id.passport_block).setVisibility(View.GONE);
+        }
     }
 }
