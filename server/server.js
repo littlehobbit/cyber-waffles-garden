e5a3a628a7c680a302f9df416055061d98550148
nkutovoy@sfedu.ru <nkutovoy@sfedu.ru>
2021-05-28 16:40:04 +0300

    update

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+var bodyParser = require("body-parser") 
 const app = express(express.json);
 
 app.listen(3000);
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     console.log("hello");
 })
+
+module.exports = app;
