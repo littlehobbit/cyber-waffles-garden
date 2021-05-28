fa63864017b7a4ed56fe3a08652e297e3a8e5b09
Can4es322 <66887951+Can4es322@users.noreply.github.com>
2021-05-28 15:39:12 +0300

    Пробная

---
+++ console/vue-taganrog-oborona/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
