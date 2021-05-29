c60e60dfd558aa8cee99f8baa4fc35bb43b54855
nkutovoy@sfedu.ru <nkutovoy@sfedu.ru>
2021-05-29 03:14:44 +0300

    yeah

---
+++ server/mysqlcon.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "192.168.43.143",
+    user: "waffles",
+    database: "deftagan",
+    password: "waffle",
+    port: "3306"
+});
+
+connection.connect(function(err){
+    if (err) {
+      console.log('Bad database connection')
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+ });
+
+ module.exports = connection;
